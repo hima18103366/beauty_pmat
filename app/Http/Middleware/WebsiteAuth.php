c507bfbd87fafbfbb61eb14f89f903cc1d0db434
MacBook <macbook@MacBooks-MacBook-Pro.local>
2023-08-19 01:25:44 +0600

    update

---
+++ app/Http/Middleware/WebsiteAuth.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Auth\Middleware\Authenticate as Middleware;
+
+class WebsiteAuth extends Middleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    protected function redirectTo($request)
+    {
+       
+        if(!auth()->user())
+        {
+            return route('hlogin');
+        }
+       
+    }
+}
