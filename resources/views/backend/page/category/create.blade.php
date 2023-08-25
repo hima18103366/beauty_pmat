ed3439cf09a3f3bc9a21701a61ae65377884f413
zidankhan86 <zidankhan718@gmail.com>
2023-08-25 21:30:39 +0600

    up to date

--- resources/views/backend/page/category/create.blade.php
+++ resources/views/backend/page/category/create.blade.php
@@ -11,18 +11,18 @@
 <form action="{{route('category.store')}}" method="post">
   @csrf
 <label for="name">Name:</label>
-    <input class="form-control" type="name" id="name" name="name" value="{{$category->name}}" required><br>
+    <input class="form-control" type="name" id="name" name="name"  required><br>
     <br>
 
     <label for="name">Details:</label>
-    <input class="form-control" type="details" id="details" name="details" value="{{$category->details}}" required><br>
+    <input class="form-control" type="details" id="details" name="details"  required><br>
     <br>
 
     <label for="name">Status:</label>
-    <input class="form-control" type="status" id="status" name="status" value="{{$category->status}}" required><br>
+    <input class="form-control" type="status" id="status" name="status" required><br>
     <br>
-  
-    
+
+
   <button class="btn btn-primary" type="submit">Submit form</button>
 </form>
 @endsection
--- resources/views/backend/page/customer/edit.blade.php
+++ resources/views/backend/page/customer/edit.blade.php
@@ -3,11 +3,11 @@
 <html>
 
 <body>
-   
 
-<form action="{{route('customer.update',$customer->id)}}" method="POST">
+
+<form action="{{route('customer.update',$customer->id)}}" method="POST" enctype="multipart/form-data">
   @csrf
-  @method='put'
+
     <label for="name">Name:</label>
     <input class="form-control" type="text" id="name" name="name" value="{{$customer->name}}" required><br>
     <br>
@@ -15,17 +15,17 @@
     <label for="phone">Phone Num :</label>
     <input class="form-control" type="tel" id="Phone num" name="phone" value="{{$customer->phpne_num}}" required><br>
     <br>
-    
+
     <label for="email">Email:</label>
     <input class="form-control" type="email" id="email" name="email" value="{{$customer->email}}" required><br>
     <br>
-    
-    
+
+
     <label for="date">Last Visit:</label>
     <input class="form-control" type="date" id="date" name="LastVisit" value="{{$customer->date}}" required><br>
     <br>
-    
-    
+
+
     <br>
     <input class="btn btn-primary" type="submit" value="Submit">
   </form>
